bd99e79f0ecc1b10cdca936e5b4888d215019d03
paulina szylin <paulina.szylin.programista@gmail.com>
2022-08-02 20:00:05 +0200

    Added the onOperator functionality

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -8,7 +8,7 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private var tvInput : TextView? = null
+    private var tvInput: TextView? = null
     private var lastNumeric: Boolean = false
     private var lastDot: Boolean = false
 
@@ -19,21 +19,44 @@ class MainActivity : AppCompatActivity() {
         tvInput = findViewById(R.id.tvInput)
     }
 
-    fun onDigit(view: View){
+    fun onDigit(view: View) {
         tvInput?.append((view as Button).text)
         lastNumeric = true
         lastDot = false
     }
 
-    fun onClear(view: View){
+    fun onClear(view: View) {
         tvInput?.text = ""
     }
 
-    fun onDecimalPoint(view: View){
-        if(lastNumeric && !lastDot){
+    fun onDecimalPoint(view: View) {
+        if (lastNumeric && !lastDot) {
             tvInput?.append(".")
             lastNumeric = false
             lastDot = true
+        }
+    }
+
+    fun onOperator(view : View){
+
+        tvInput?.text?.let{
+            if(lastNumeric && !isOperatorAdded(it.toString())){
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("*")
         }
     }
 }
