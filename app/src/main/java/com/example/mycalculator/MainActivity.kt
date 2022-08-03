c194b17f43765de69afaf2afa08899e122ae27ea
paulina szylin <paulina.szylin.programista@gmail.com>
2022-08-03 11:52:03 +0200

    Added subtraction

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -37,16 +37,45 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun onOperator(view : View){
+    fun onOperator(view: View) {
 
-        tvInput?.text?.let{
-            if(lastNumeric && !isOperatorAdded(it.toString())){
+        tvInput?.text?.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
                 tvInput?.append((view as Button).text)
                 lastNumeric = false
                 lastDot = false
             }
         }
 
+    }
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+
+                if (tvValue.contains("-")) {
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+
+                    if (prefix.isNotEmpty()) {
+                        one = prefix + one
+                    }
+
+                    tvInput?.text = (one.toDouble() - two.toDouble()).toString()
+                }
+
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
     }
 
     private fun isOperatorAdded(value: String): Boolean {
